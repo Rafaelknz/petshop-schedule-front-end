578ed6ce91041ed357c9838aeaaaef115c8194fc
allanrot <allantrickrothenburg@gmail.com>
2021-01-04 17:16:24 -0300

    update

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -31,7 +31,7 @@
     name: 'Home',
 
     data: () => ({
-    
+
     }),
 
     created() {
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 import Home from './components/home/Home.vue'
+import Calendar from './components/calendar/Calendar.vue'
+
 export const routes = [
-    { path: '', component: Home, titulo: 'Home' }
+    { path: '', component: Home, titulo: 'Home' },
+    { path: '/admin/index', component: Calendar, titulo: 'Calendar' }
+
 ];
